32732bce1db05cfebfd4218ec636703e462493df
ChangUk07 <xncnwk@gmail.com>
2024-11-08 14:51:41 +0900

    0.1.8 merge auth 0.2.2 - merge missing files when merging

--- HappyPaws/src/main/java/com/happypaws/svc/AuthApiSVC.java
+++ HappyPaws/src/main/java/com/happypaws/svc/AuthApiSVC.java
@@ -9,18 +9,29 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
+import javax.annotation.PreDestroy;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -30,42 +41,98 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.happypaws.util.Argon2Util;
 import com.happypaws.vo.UsersVO;
 
-@Service
-public class AuthApiSVC {
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
 
-    @Autowired
-    private ServletContext servletContext;
-	
+@Service
+public class AuthApiSVC implements InitializingBean {
+	@Autowired
+	private ServletContext servletContext;
+
+	// ========== naver ========== //
 	@Value("${spring.security.oauth2.client.registration.naver.client-id}")
 	private String naverClientId;
-
 	@Value("${spring.security.oauth2.client.registration.naver.client-secret}")
 	private String naverClientSecret;
-
+	@Value("${spring.security.oauth2.client.provider.naver.authorization-uri}")
+	private String naverAuthorizationUri;
 	@Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
-	private String naverCallbackUrl;
+	private String naverRedirectUri;
+	@Value("${spring.security.oauth2.client.provider.naver.token-uri}")
+	private String naverTokenUri;
+	@Value("${spring.security.oauth2.client.provider.naver.user-info-uri}")
+	private String naverUserInfoUrl;
 
-	public String naverLoginUrl(HttpServletRequest request) throws UnsupportedEncodingException {
-		String clientId = naverClientId;
-		String redirectURI = URLEncoder.encode(naverCallbackUrl, "UTF-8");
-		String state = UUID.randomUUID().toString();
-		request.getSession().setAttribute("naverOauthState", state);
+	// ========== kakao ========== //
 
-		String apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code"
-			+ "&client_id=" + clientId
-			+ "&state=" + state
-			+ "&redirect_uri=" + redirectURI;
+	// ========== kakao ========== //
+	@Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+	private String kakaoClientId;
+	@Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+	private String kakaoClientSecret;
+	@Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}")
+	private String kakaoAuthorizationUri;
+	@Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+	private String kakaoRedirectUri;
+	@Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+	private String kakaoTokenUri;
+	@Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+	private String kakaoUserInfoUri;
 
-		return apiURL;
+	// ========== coolsms ========== //
+	private DefaultMessageService messageService;
+	private final Map<String, String> verificationCodeStore = new HashMap<>();
+	private final Map<String, ScheduledFuture<?>> scheduledTasks = new HashMap<>();
+	private final SecureRandom random = new SecureRandom();
+	private final ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+
+	@Value("${spring.security.oauth2.client.registration.coolsms.client-id}")
+	private String coolsmsApiKey;
+	@Value("${spring.security.oauth2.client.registration.coolsms.client-secret}")
+	private String coolsmsApiSecret;
+	@Value("${spring.security.oauth2.client.registration.coolsms.sender-number}")
+	private String coolsmsSendNumber;
+
+	public AuthApiSVC() {
+		taskScheduler.initialize();
 	}
-	
-	public String requestNaverAccessToken(String code, String state) throws JsonMappingException, JsonProcessingException {
-		String tokenUrl = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code"
+
+	// ========== login divider common ========== //
+	@Value("${spring.security.oauth2.client.registration.divider.authorization-grant-type}")
+	private String grantType;
+
+	public void snsLoginUrl(HttpServletRequest request) throws UnsupportedEncodingException {
+		String state = UUID.randomUUID().toString();
+		request.getSession().setAttribute("oauthState", state);
+
+		String naverLoginUrl = naverAuthorizationUri
+			+ "?response_type=code"
+			+ "&client_id=" + naverClientId
+			+ "&state=" + state
+			+ "&redirect_uri=" + URLEncoder.encode(naverRedirectUri, "UTF-8");
+
+		String kakaoLoginUrl = kakaoAuthorizationUri
+			+ "?response_type=code"
+			+ "&client_id=" + kakaoClientId
+			+ "&state=" + state
+			+ "&redirect_uri=" + URLEncoder.encode(kakaoRedirectUri, "UTF-8");
+
+		request.setAttribute("naverLoginUrl", naverLoginUrl);
+		request.setAttribute("kakaoLoginUrl", kakaoLoginUrl);
+	}
+
+	// ========== method by login divider ========== //
+	public UsersVO requestNaverUserDetails(String code, String state) throws JsonMappingException, JsonProcessingException, UnsupportedEncodingException {
+		// request token
+		String tokenUrl = naverTokenUri
+			+ "?grant_type=" + grantType
 			+ "&client_id=" + naverClientId
 			+ "&client_secret=" + naverClientSecret
 			+ "&code=" + code
 			+ "&state=" + state;
-	
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.exchange(
 			tokenUrl,
@@ -73,19 +140,20 @@ public class AuthApiSVC {
 			new HttpEntity<>(new HttpHeaders()),
 			String.class
 		);
-		
-		return (new ObjectMapper()).readTree(response.getBody()).path("access_token").asText();
-	}
 
-    public UsersVO requestUserProfile(String accessToken) throws JsonMappingException, JsonProcessingException, UnsupportedEncodingException {
-		String profileUrl = "https://openapi.naver.com/v1/nid/me";
-	
+		String accessToken = (new ObjectMapper()).readTree(response.getBody()).path("access_token").asText();
+
+		if (accessToken == null) {
+			return null;
+		}
+
+		// request profile
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Authorization", "Bearer " + accessToken);
 	
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.exchange(
-			profileUrl,
+		restTemplate = new RestTemplate();
+		response = restTemplate.exchange(
+			naverUserInfoUrl,
 			HttpMethod.GET,
 			new HttpEntity<>(headers),
 			String.class
@@ -101,6 +169,53 @@ public class AuthApiSVC {
 		user.setUs_email(responseNode.path("email").asText());
 		user.setUs_phone(responseNode.path("mobile").asText());
 		user.setUs_profile(responseNode.path("profile_image").asText().replaceAll("\\/", "/"));
+
+		return user;
+	}
+
+	public UsersVO requestKakaoUserDetails(String code) throws JsonMappingException, JsonProcessingException, UnsupportedEncodingException {
+		// request token
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", grantType);
+		params.add("client_id", kakaoClientId);
+		params.add("redirect_uri", kakaoRedirectUri);
+		params.add("code", code);
+		params.add("client_secret", kakaoClientSecret);
+
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+		ResponseEntity<String> response = restTemplate.postForEntity(kakaoTokenUri, request, String.class);
+
+		String accessToken = (new ObjectMapper()).readTree(response.getBody()).path("access_token").asText();
+
+		if (accessToken == null) {
+			return null;
+		}
+
+		// request profile
+		headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + accessToken);
+
+		restTemplate = new RestTemplate();
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+
+		response = restTemplate.exchange(kakaoUserInfoUri, HttpMethod.GET, entity, String.class);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		UsersVO user = null;
+		JsonNode rootNode = objectMapper.readTree(response.getBody());
+		JsonNode kakaoAccountNode = rootNode.path("kakao_account");
+		user = new UsersVO();
+		user.setUs_id(rootNode.path("id").asText());
+		user.setUs_sns("kakao");
+		user.setUs_password(Argon2Util.hashPassword(Argon2Util.hashPassword(accessToken)));
+		user.setUs_name(kakaoAccountNode.path("name").asText());
+		user.setUs_nick(kakaoAccountNode.path("profile").path("nickname").asText());
+		user.setUs_email(kakaoAccountNode.path("email").asText());
+		user.setUs_phone(kakaoAccountNode.path("phone_number").asText());
+		user.setUs_profile(kakaoAccountNode.path("profile").path("thumbnail_image_url").asText());
 		return user;
 	}
 
@@ -110,7 +225,6 @@ public class AuthApiSVC {
 			user.setUs_profile("default.jpg");
 			return;
 		}
-		
 		String fileName = user.getUs_id();
 
 		try {
@@ -120,14 +234,14 @@ public class AuthApiSVC {
 				fileName = fileName + fileExtension;
 			}
 
-            // webapp/resources/uploads 폴더의 절대 경로 가져오기
-            String uploadDirPath = servletContext.getRealPath("/resources/profile_images/");
-            File uploadDir = new File(uploadDirPath);
-            if (!uploadDir.exists()) {
-                uploadDir.mkdirs(); // 디렉토리가 없으면 생성
-            }
+			// webapp/resources/uploads 폴더의 절대 경로 가져오기
+			String uploadDirPath = servletContext.getRealPath("/resources/profile_images/");
+			File uploadDir = new File(uploadDirPath);
+			if (!uploadDir.exists()) {
+				uploadDir.mkdirs(); // 디렉토리가 없으면 생성
+			}
 
-            File saveFile = new File(uploadDir, fileName);
+			File saveFile = new File(uploadDir, fileName);
 
 			// URL로부터 연결을 생성하여 이미지 다운로드
 			URL url = new URL(imageUrl);
@@ -148,5 +262,55 @@ public class AuthApiSVC {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	// ========== cool sms methods ========== //
+	@Override
+	public void afterPropertiesSet() {
+		this.messageService = NurigoApp.INSTANCE.initialize(coolsmsApiKey, coolsmsApiSecret, "https://api.coolsms.co.kr");
+	}
+
+	public String generateAndSendCode(String phoneNumber) {
+		String code = String.format("%06d", random.nextInt(1000000));
+		verificationCodeStore.put(phoneNumber, code);
+
+		// 인증 코드는 5분 동안 유효
+		ScheduledFuture<?> scheduledTask = taskScheduler.schedule(() -> {
+			verificationCodeStore.remove(phoneNumber);
+			scheduledTasks.remove(phoneNumber);
+		}, new java.util.Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(5)));
+		scheduledTasks.put(phoneNumber, scheduledTask);
+
+		Message message = new Message();
+		message.setFrom(coolsmsSendNumber);
+		message.setTo(phoneNumber);
+		message.setText("HappyPaws\n인증번호는 [" + code + "] 입니다.");
+
+		try {
+			SingleMessageSentResponse response = messageService.sendOne(new SingleMessageSendingRequest(message));
+            System.out.println("메시지 전송 성공: " + response.getMessageId());
+			return response.getMessageId();
+		} catch (Exception e) {
+            System.err.println("메시지 전송 실패: " + e.getMessage());
+			return null;
+		}
+	}
+
+	public boolean verifyCode(String phoneNumber, String code) {
+		String storedCode = verificationCodeStore.get(phoneNumber);
+		if (storedCode != null && storedCode.equals(code)) {
+			ScheduledFuture<?> scheduledTask = scheduledTasks.remove(phoneNumber);
+			if (scheduledTask != null) {
+				scheduledTask.cancel(true);
+			}
+			verificationCodeStore.remove(phoneNumber);
+			return true;
+		}
+		return false;
+	}
+
+	@PreDestroy
+	public void cleanUp() {
+		taskScheduler.shutdown();
 	}
 }
